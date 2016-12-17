e935d105bf3a88f0c2e43c0fe97dd9af47b21481
Piotr Gankiewicz <spetz90@gmail.com>
2016-12-17 19:09:10 +0100

    User session storage

---
+++ Warden.Services.Storage/Modules/UserSessionModule.cs
@@ -0,0 +1,15 @@
+﻿using Warden.Services.Storage.Providers;
+using Warden.Services.Storage.Queries;
+using Warden.Services.Users.Shared.Dto;
+
+namespace Warden.Services.Storage.Modules
+{
+    public class UserSessionModule : ModuleBase
+    {
+        public UserSessionModule(IUserProvider userProvider) : base("user-sessions")
+        {
+            Get("{id}", async args => await Fetch<GetUserSession, UserSessionDto>
+                (async x => await userProvider.GetSessionAsync(x.Id)).HandleAsync());
+        }
+    }
+}
