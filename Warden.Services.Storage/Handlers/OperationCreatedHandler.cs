3b4f6dabb96b8753ccc4c44189c9dd4ce97a8097
Piotr Gankiewicz <spetz90@gmail.com>
2017-01-09 21:00:27 +0100

    Fixed operation data

--- Warden.Services.Storage/Handlers/OperationCreatedHandler.cs
+++ Warden.Services.Storage/Handlers/OperationCreatedHandler.cs
@@ -3,40 +3,31 @@ using System.Threading.Tasks;
 using Warden.Common.Events;
 using Warden.Services.Operations.Shared.Dto;
 using Warden.Services.Operations.Shared.Events;
-using Warden.Services.Storage.Providers;
 using Warden.Services.Storage.Repositories;
 
 namespace Warden.Services.Storage.Handlers
 {
     public class OperationCreatedHandler : IEventHandler<OperationCreated>
     {
-        private readonly IOperationProvider _operationProvider;
         private readonly IOperationRepository _operationRepository;
 
-        public OperationCreatedHandler(IOperationProvider operationProvider, IOperationRepository operationRepository)
+        public OperationCreatedHandler(IOperationRepository operationRepository)
         {
-            _operationProvider = operationProvider;
             _operationRepository = operationRepository;
         }
 
         public async Task HandleAsync(OperationCreated @event)
         {
-            var operation = await _operationProvider.GetAsync(@event.RequestId);
-            if(operation.HasNoValue)
-                return;
-
             await _operationRepository.AddAsync(new OperationDto
             {
-                Id = operation.Value.Id,
+                Id = Guid.NewGuid(),
                 RequestId = @event.RequestId,
+                Name = @event.Name,
                 UserId = @event.UserId,
                 Origin = @event.Origin,
                 Resource = @event.Resource,
                 State = @event.State,
-                Code = operation.Value.Code,
-                Message = operation.Value.Message,
                 CreatedAt = @event.CreatedAt,
-                UpdatedAt = operation.Value.UpdatedAt
             });
         }
     }
