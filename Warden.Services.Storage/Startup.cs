89f3bde9af2fd3e14d13879a525339cbff10d44b
spetz <spetz90@gmail.com>
2017-02-07 12:34:25 +0100

    Updated project.json packages and NLog configuration.

--- Warden.Services.Storage/Startup.cs
+++ Warden.Services.Storage/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Nancy.Owin;
 using NLog.Extensions.Logging;
+using NLog.Web;
 using Warden.Services.Storage.Framework;
 
 namespace Warden.Services.Storage
