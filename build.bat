4caa4cd9e9adeb4ae3ab74ec9f247236dea55fb6
spetz <spetz90@gmail.com>
2017-03-16 14:51:28 +0100

    Updated framework

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-dotnet restore --source "https://api.nuget.org/v3/index.json" --source "https://www.myget.org/F/warden%build-env%/api/v3/index.json" --no-cache
+dotnet restore Warden.Services.Storage.Models --source "https://api.nuget.org/v3/index.json" --source "https://www.myget.org/F/warden%build-env%/api/v3/index.json" --no-cache
 dotnet pack "Warden.Services.Storage.Models" -o .
